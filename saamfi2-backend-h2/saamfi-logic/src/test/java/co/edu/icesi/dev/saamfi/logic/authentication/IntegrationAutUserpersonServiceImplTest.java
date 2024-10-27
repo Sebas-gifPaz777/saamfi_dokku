14cce93d66714f3e277b6a98a4c0f4ec2d072cdb
Sebas-gifPaz777 <pazsebas797@gmail.com>
2024-10-27 18:45:07 -0500

    first commit

---
+++ saamfi2-backend-h2/saamfi-logic/src/test/java/co/edu/icesi/dev/saamfi/logic/authentication/IntegrationAutUserpersonServiceImplTest.java
@@ -0,0 +1,16 @@
+package co.edu.icesi.dev.saamfi.logic.authentication;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+public class IntegrationAutUserpersonServiceImplTest {
+
+	@Test
+	@Disabled
+	public void getUsersbyPerson() throws Exception {
+
+	}
+
+}
